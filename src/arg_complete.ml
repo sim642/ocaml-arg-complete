1773cffbe58d1ef2a6eace3b89370a12e5aa4e8c
Simmo Saan <simmo.saan@gmail.com>
2022-03-26 15:35:04 +0200

    Implement missing simple Arg.spec cases

--- src/arg_complete.ml
+++ src/arg_complete.ml
@@ -2,34 +2,69 @@ type complete = string -> string list
 
 type spec =
   | Unit of (unit -> unit)
+  | Bool of (bool -> unit)
+  | Set of bool ref
+  | Clear of bool ref
   | String of (string -> unit) * complete
+  | Set_string of string ref * complete
+  | Int of (int -> unit) * complete
+  | Set_int of int ref * complete
+  | Float of (float -> unit) * complete
+  | Set_float of float ref * complete
 
 type arg_speclist = (Arg.key * Arg.spec * Arg.doc) list
 type speclist = (Arg.key * spec * Arg.doc) list
 
 let arg_spec: spec -> Arg.spec = function
   | Unit f -> Arg.Unit f
+  | Bool f -> Arg.Bool f
+  | Set r -> Arg.Set r
+  | Clear r -> Arg.Clear r
   | String (f, _c) -> Arg.String f
+  | Set_string (r, _c) -> Arg.Set_string r
+  | Int (f, _c) -> Arg.Int f
+  | Set_int (r, _c) -> Arg.Set_int r
+  | Float (f, _c) -> Arg.Float f
+  | Set_float (r, _c) -> Arg.Set_float r
 
 let arg_speclist: speclist -> arg_speclist = fun l ->
   List.map (fun (k, sc, d) -> (k, arg_spec sc, d)) l
+
+let complete_strings l s =
+  List.filter (String.starts_with ~prefix:s) l
 
 let complete_argv (argv: string array) (speclist: speclist) (_anon_complete: complete): string list =
   let rec complete_arg (argv: string list) =
     match argv with
     | [] -> []
     | arg :: argv' ->
-      List.concat_map (fun (key, spec, _doc) ->
-          if arg = key then (
-            match spec, argv' with
-            | Unit _f, argv' -> complete_arg argv'
-            | String (_f, c), arg' :: _ -> c arg'
-            | _, _ -> []
-          )
-          else if String.starts_with ~prefix:arg key then
-            [key]
-          else
-            []
-        ) speclist
+      try
+        let (_, spec, _) = List.find (fun (key, _, _) -> arg = key) speclist in
+        match spec, argv' with
+        | Unit _f, argv' -> complete_arg argv'
+        | Bool _f, [arg'] -> complete_strings ["false"; "true"] arg'
+        | Bool _f, _ :: argv' -> complete_arg argv'
+        | Set _r, argv' -> complete_arg argv'
+        | Clear _r, argv' -> complete_arg argv'
+        | String (_f, c), [arg'] -> c arg'
+        | String (_f, _c), _ :: argv' -> complete_arg argv'
+        | Set_string (_r, c), [arg'] -> c arg'
+        | Set_string (_r, _c), _ :: argv' -> complete_arg argv'
+        | Int (_f, c), [arg'] -> c arg'
+        | Int (_f, _c), _ :: argv' -> complete_arg argv'
+        | Set_int (_r, c), [arg'] -> c arg'
+        | Set_int (_r, _c), _ :: argv' -> complete_arg argv'
+        | Float (_f, c), [arg'] -> c arg'
+        | Float (_f, _c), _ :: argv' -> complete_arg argv'
+        | Set_float (_r, c), [arg'] -> c arg'
+        | Set_float (_r, _c), _ :: argv' -> complete_arg argv'
+        | _, _ -> failwith "cannot complete"
+      with Not_found ->
+        List.filter_map (fun (key, _spec, _doc) ->
+            if String.starts_with ~prefix:arg key then
+              Some key
+            else
+              None
+          ) speclist
   in
   complete_arg (List.tl (Array.to_list argv))
--- test/arg_complete_test.ml
+++ test/arg_complete_test.ml
@@ -2,7 +2,11 @@ open OUnit2
 
 let speclist: (Arg.key * Arg_complete.spec * Arg.doc) list = [
   ("--unit", Unit (fun () -> print_endline "unit"), "Unit");
-  ("--string", String ((fun s -> Printf.printf "string: %s" s), (fun _s -> ["a"; "b"; "c"])), "String");
+  ("--bool", Bool (fun b -> Printf.printf "bool: %B" b), "Bool");
+  ("--set", Set (ref false), "Set");
+  ("--clear", Clear (ref false), "Clear");
+  ("--string", String ((fun s -> Printf.printf "string: %s" s), (Arg_complete.complete_strings ["a"; "b"; "c"])), "String");
+  ("--set_string", Set_string (ref "", (Arg_complete.complete_strings ["a"; "b"; "c"])), "Set_string");
 ]
 
 let anon_fun: Arg.anon_fun = fun s ->
@@ -16,24 +20,48 @@ let usage_msg: Arg.usage_msg = "usage_msg"
 (* let () =
   Arg.parse_argv [|"program"; "--unit"|] speclist anon_fun usage_msg *)
 
+let all_keys = List.map (fun (key, _, _) -> key) speclist
+
 let test_key _ =
   assert_equal ["--unit"] (Arg_complete.complete_argv [|"program"; "--uni"|] speclist anon_complete);
   assert_equal ["--string"] (Arg_complete.complete_argv [|"program"; "--str"|] speclist anon_complete);
-  assert_equal ["--unit"; "--string"] (Arg_complete.complete_argv [|"program"; "--"|] speclist anon_complete);
-  assert_equal ["--unit"; "--string"] (Arg_complete.complete_argv [|"program"; ""|] speclist anon_complete);
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--"|] speclist anon_complete);
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; ""|] speclist anon_complete);
   assert_equal [] (Arg_complete.complete_argv [|"program"; "--unit"|] speclist anon_complete)
 
 let test_unit _ =
   assert_equal ["--unit"] (Arg_complete.complete_argv [|"program"; "--unit"; "--un"|] speclist anon_complete)
 
+let test_bool _ =
+  assert_equal ["false"; "true"] (Arg_complete.complete_argv [|"program"; "--bool"; ""|] speclist anon_complete);
+  assert_equal ["true"] (Arg_complete.complete_argv [|"program"; "--bool"; "t"|] speclist anon_complete);
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--bool"; "true"; ""|] speclist anon_complete)
+
+let test_set _ =
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--set"; ""|] speclist anon_complete)
+
+let test_clear _ =
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--clear"; ""|] speclist anon_complete)
+
 let test_string _ =
-  assert_equal ["a"; "b"; "c"] (Arg_complete.complete_argv [|"program"; "--string"; ""|] speclist anon_complete)
+  assert_equal ["a"; "b"; "c"] (Arg_complete.complete_argv [|"program"; "--string"; ""|] speclist anon_complete);
+  assert_equal ["a"] (Arg_complete.complete_argv [|"program"; "--string"; "a"|] speclist anon_complete);
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--string"; "a"; ""|] speclist anon_complete)
+
+let test_set_string _ =
+  assert_equal ["a"; "b"; "c"] (Arg_complete.complete_argv [|"program"; "--set_string"; ""|] speclist anon_complete);
+  assert_equal ["a"] (Arg_complete.complete_argv [|"program"; "--set_string"; "a"|] speclist anon_complete);
+  assert_equal all_keys (Arg_complete.complete_argv [|"program"; "--set_string"; "a"; ""|] speclist anon_complete)
 
 let tests =
   "arg_complete_test" >::: [
     "key" >:: test_key;
     "unit" >:: test_unit;
+    "bool" >:: test_bool;
+    "set" >:: test_set;
+    "clear" >:: test_clear;
     "string" >:: test_string;
+    "set_string" >:: test_set_string;
   ]
 
 let () = run_test_tt_main tests
