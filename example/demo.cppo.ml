7f19578e91f0db562b6a01afc8edc57766c63423
Simmo Saan <simmo.saan@gmail.com>
2022-03-27 13:47:31 +0300

    Fix example OCaml < 4.12 compatibility

--- example/demo.cppo.ml
+++ example/demo.cppo.ml
@@ -16,16 +16,38 @@ let rec speclist: (Arg.key * Arg_complete.spec * Arg.doc) list = [
   ("--symbol", Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s)), "Symbol");
   ("--tuple", Tuple [Bool (fun b -> Printf.printf "bool: %B" b); Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s))], "Tuple");
   ("--", Rest ((fun s -> Printf.printf "rest: %s" s), (Arg_complete.complete_strings ["foo"; "bar"])), "Rest");
+#if OCAML_VERSION >= (4, 12, 0)
   ("-+", Rest_all ((fun _l -> Printf.printf "rest_all"), (function [arg] -> Arg_complete.complete_strings ["foo"; "bar"] arg | [_; arg] -> Arg_complete.complete_strings ["bar"] arg | _ -> failwith "too many args")), "Rest_all");
+#endif
   ("--expand", Expand (fun s -> [|s; s|]), "Expand");
+#if OCAML_VERSION >= (4, 12, 0)
   ("--complete", Rest_all (complete_all, fun _ -> failwith "complete complete"), "Complete");
+#else
+  ("--complete", Rest (complete_one, fun _ -> failwith "complete complete"), "Complete");
+#endif
   ("--file", String ((fun f -> Printf.printf "file: %s" f), complete_file), "file");
 ]
 and complete_all argv =
   let completions = Arg_complete.complete_argv argv speclist anon_complete in
   List.iter print_endline completions
+#if OCAML_VERSION < (4, 12, 0)
+and complete_one_acc = ref None
+and complete_one arg =
+  complete_one_acc := Some (
+    match !complete_one_acc with
+    | None -> [arg]
+    | Some acc -> arg :: acc
+  )
+#endif
 
 let usage_msg: Arg.usage_msg = "usage_msg"
 
 let () =
-  Arg.parse (Arg_complete.arg_speclist speclist) anon_fun usage_msg
+  Arg.parse (Arg_complete.arg_speclist speclist) anon_fun usage_msg;
+#if OCAML_VERSION < (4, 12, 0)
+  match !complete_one_acc with
+  | Some acc -> complete_all (List.rev acc)
+  | None -> ()
+#else
+  ()
+#endif
