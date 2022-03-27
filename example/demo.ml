dcc2902cf1debf2bc4249fbbd48b895b79509ff4
Simmo Saan <simmo.saan@gmail.com>
2022-03-27 16:20:51 +0300

    Simplfy demos

--- example/demo.ml
+++ example/demo.ml
@@ -1,25 +1,24 @@
-let anon_fun: Arg.anon_fun = fun s ->
-  Printf.printf "anon: %s" s
+let anon_fun: Arg.anon_fun = Printf.printf "anon: %s\n"
 
 let anon_complete: Arg_complete.complete = Arg_complete.complete_strings ["bar"; "baz"]
 
 let complete_file _s =
   [] (* -o default in script changes empty completion to default filename completion *)
 
-let rec speclist: (Arg.key * Arg_complete.spec * Arg.doc) list = [
-  ("--unit", Unit (fun () -> print_endline "unit"), "Unit");
-  ("--bool", Bool (fun b -> Printf.printf "bool: %B" b), "Bool");
-  ("--string", String ((fun s -> Printf.printf "string: %s" s), (Arg_complete.complete_strings ["a"; "b"; "c"])), "String");
-  ("--symbol", Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s)), "Symbol");
-  ("--tuple", Tuple [Bool (fun b -> Printf.printf "bool: %B" b); Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s))], "Tuple");
-  ("--complete", Rest_all (complete_all, (fun _ -> failwith "complete complete")), "Complete");
-  ("--file", String ((fun f -> Printf.printf "file: %s" f), complete_file), "file");
+let rec speclist: Arg_complete.speclist = [
+  ("--unit", Unit (fun () -> Printf.printf "unit\n"), "Unit");
+  ("--bool", Bool (Printf.printf "bool: %B\n"), "Bool");
+  ("--string", String (Printf.printf "string: %s\n", Arg_complete.complete_strings ["a"; "b"; "c"]), "String");
+  ("--tuple", Tuple [Bool (Printf.printf "tuple bool: %B\n"); Symbol (["a"; "b"; "c"], Printf.printf "tuple symbol: %s\n")], "Tuple");
+  ("--file", String (Printf.printf "file: %s\n", complete_file), "file");
+  ("--complete", Rest_all (complete, (fun _ -> failwith "complete complete")), "Complete");
 ]
-and complete_all argv =
-  let completions = Arg_complete.complete_argv argv speclist anon_complete in
-  List.iter print_endline completions
+and complete args =
+  Arg_complete.complete_argv args speclist anon_complete
+  |> List.iter print_endline
 
-let usage_msg: Arg.usage_msg = "usage_msg"
 
 let () =
-  Arg.parse (Arg_complete.arg_speclist speclist) anon_fun usage_msg
+  let arg_speclist = Arg_complete.arg_speclist speclist in
+  let usage_msg = "Usage:" in
+  Arg.parse arg_speclist anon_fun usage_msg
--- example/demo_compat.ml
+++ example/demo_compat.ml
@@ -1,27 +1,26 @@
-let anon_fun: Arg.anon_fun = fun s ->
-  Printf.printf "anon: %s" s
+let anon_fun: Arg.anon_fun = Printf.printf "anon: %s\n"
 
 let anon_complete: Arg_complete.complete = Arg_complete.complete_strings ["bar"; "baz"]
 
 let complete_file _s =
   [] (* -o default in script changes empty completion to default filename completion *)
 
-let rec speclist: (Arg.key * Arg_complete.spec * Arg.doc) list Lazy.t = lazy [
-  ("--unit", Unit (fun () -> print_endline "unit"), "Unit");
-  ("--bool", Bool (fun b -> Printf.printf "bool: %B" b), "Bool");
-  ("--string", String ((fun s -> Printf.printf "string: %s" s), (Arg_complete.complete_strings ["a"; "b"; "c"])), "String");
-  ("--symbol", Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s)), "Symbol");
-  ("--tuple", Tuple [Bool (fun b -> Printf.printf "bool: %B" b); Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s))], "Tuple");
+let rec speclist: Arg_complete.speclist Lazy.t = lazy [
+  ("--unit", Unit (fun () -> Printf.printf "unit\n"), "Unit");
+  ("--bool", Bool (Printf.printf "bool: %B\n"), "Bool");
+  ("--string", String (Printf.printf "string: %s\n", Arg_complete.complete_strings ["a"; "b"; "c"]), "String");
+  ("--tuple", Tuple [Bool (Printf.printf "tuple bool: %B\n"); Symbol (["a"; "b"; "c"], Printf.printf "tuple symbol: %s\n")], "Tuple");
+  ("--file", String (Printf.printf "file: %s\n", complete_file), "file");
   ("--complete", Arg_complete.Rest_all_compat.spec (Lazy.force rest_all_complete), "Complete");
-  ("--file", String ((fun f -> Printf.printf "file: %s" f), complete_file), "file");
 ]
-and rest_all_complete = lazy (Arg_complete.Rest_all_compat.create complete_all (fun _ -> failwith "complete complete"))
-and complete_all argv =
-  let completions = Arg_complete.complete_argv argv (Lazy.force speclist) anon_complete in
-  List.iter print_endline completions
+and rest_all_complete = lazy (Arg_complete.Rest_all_compat.create complete (fun _ -> failwith "complete complete"))
+and complete args =
+  Arg_complete.complete_argv args (Lazy.force speclist) anon_complete
+  |> List.iter print_endline
 
-let usage_msg: Arg.usage_msg = "usage_msg"
 
 let () =
-  Arg.parse (Arg_complete.arg_speclist (Lazy.force speclist)) anon_fun usage_msg;
+  let arg_speclist = Arg_complete.arg_speclist (Lazy.force speclist) in
+  let usage_msg = "Usage:" in
+  Arg.parse arg_speclist anon_fun usage_msg;
   Arg_complete.Rest_all_compat.finish (Lazy.force rest_all_complete)
