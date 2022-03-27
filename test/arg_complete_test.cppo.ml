5710685553ddb8cb70d8c138f027a9c377fef18a
Simmo Saan <simmo.saan@gmail.com>
2022-03-27 16:29:31 +0300

    Simplfy speclist in tests as well

--- test/arg_complete_test.cppo.ml
+++ test/arg_complete_test.cppo.ml
@@ -1,15 +1,18 @@
 open OUnit2
 
-let speclist: (Arg.key * Arg_complete.spec * Arg.doc) list = [
-  ("--unit", Unit (fun () -> print_endline "unit"), "Unit");
-  ("--bool", Bool (fun b -> Printf.printf "bool: %B" b), "Bool");
+let anon_fun: Arg.anon_fun = Printf.printf "anon: %s\n"
+let anon_complete: Arg_complete.complete = Arg_complete.strings ["bar"; "baz"]
+
+let speclist: Arg_complete.speclist = [
+  ("--unit", Unit (fun () -> print_endline "unit\n"), "Unit");
+  ("--bool", Bool (Printf.printf "bool: %B"), "Bool");
   ("--set", Set (ref false), "Set");
   ("--clear", Clear (ref false), "Clear");
-  ("--string", String ((fun s -> Printf.printf "string: %s" s), (Arg_complete.strings ["a"; "b"; "c"])), "String");
-  ("--set_string", Set_string (ref "", (Arg_complete.strings ["a"; "b"; "c"])), "Set_string");
-  ("--symbol", Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s)), "Symbol");
-  ("--tuple", Tuple [Bool (fun b -> Printf.printf "bool: %B" b); Symbol (["a"; "b"; "c"], (fun s -> Printf.printf "symbol: %s" s))], "Tuple");
-  ("--", Rest ((fun s -> Printf.printf "rest: %s" s), (Arg_complete.strings ["foo"; "bar"])), "Rest");
+  ("--string", String (Printf.printf "string: %s\n", Arg_complete.strings ["a"; "b"; "c"]), "String");
+  ("--set_string", Set_string (ref "", Arg_complete.strings ["a"; "b"; "c"]), "Set_string");
+  ("--symbol", Symbol (["a"; "b"; "c"], Printf.printf "symbol: %s\b"), "Symbol");
+  ("--tuple", Tuple [Bool (Printf.printf "tuple bool: %B\n"); Symbol (["a"; "b"; "c"], Printf.printf "tuple symbol: %s\n")], "Tuple");
+  ("--", Rest (Printf.printf "rest: %s\n", Arg_complete.strings ["foo"; "bar"]), "Rest");
 #if OCAML_VERSION >= (4, 12, 0)
   ("-+", Rest_all ((fun _l -> Printf.printf "rest_all"), (function [arg] -> Arg_complete.strings ["foo"; "bar"] arg | [_; arg] -> Arg_complete.strings ["bar"] arg | _ -> failwith "too many args")), "Rest_all");
 #endif
@@ -19,23 +22,15 @@ let speclist: (Arg.key * Arg_complete.spec * Arg.doc) list = [
   ("--side_effect",
     (let tmp = ref "" in
     let spec1 = Arg_complete.Set_string (tmp, (fun s -> tmp := s; [])) in
-    let spec2 = Arg_complete.String ((fun s -> Printf.printf "side_effect: %s" s), (fun _ -> [!tmp])) in
+    let spec2 = Arg_complete.String (Printf.printf "side_effect: %s\n", (fun _ -> [!tmp])) in
     Tuple [spec1; spec2]),
     "side effect");
 ]
 
-let anon_fun: Arg.anon_fun = fun s ->
-  Printf.printf "anon: %s" s
-
-let anon_complete: Arg_complete.complete = Arg_complete.strings ["bar"; "baz"]
-
-let usage_msg: Arg.usage_msg = "usage_msg"
-
-(* let () =
-  Arg.parse_argv ["--unit"] speclist anon_fun usage_msg *)
 
 let all_keys = List.map (fun (key, _, _) -> key) speclist
 let all_empty = ["bar"; "baz"]
+
 
 let test_anon _ =
   assert_equal ["bar"; "baz"] (Arg_complete.complete_argv ["b"] speclist anon_complete);
@@ -106,6 +101,7 @@ let test_side_effect _ =
   assert_equal ~printer [] (Arg_complete.complete_argv ["--side_effect"; ""] speclist anon_complete);
   assert_equal ~printer ["foo"] (Arg_complete.complete_argv ["--side_effect"; "foo"; ""] speclist anon_complete);
   assert_equal ~printer all_empty (Arg_complete.complete_argv ["--side_effect"; "foo"; "foo"; ""] speclist anon_complete)
+
 
 let tests =
   "arg_complete_test" >::: [
