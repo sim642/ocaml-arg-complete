1615464b4d9dbc29f02f3f7cf429e1c1165201f0
Simmo Saan <simmo.saan@gmail.com>
2025-02-01 22:58:30 +0200

    Bypass = in COMP_WORDBREAKS in demo-completion.sh

--- example/demo-completion.sh
+++ example/demo-completion.sh
@@ -6,10 +6,27 @@
 # Permanent usage:
 #   Run: echo "source $(readlink -f .)/demo-completion.sh" >> ~/.bash_completion
 
+# Bypass = in COMP_WORDBREAKS (https://stackoverflow.com/a/57437406/854540)
+# Copied & modified from standard __ltrim_colon_completions
+__ltrim_equal_completions()
+{
+    if [[ $1 == *=* && $COMP_WORDBREAKS == *=* ]]; then
+        # Remove equal-word prefix from COMPREPLY items
+        local colon_word=${1%"${1##*=}"}
+        local i=${#COMPREPLY[*]}
+        while ((i-- > 0)); do
+            COMPREPLY[i]=${COMPREPLY[i]#"$colon_word"}
+        done
+    fi
+}
+
 _demo ()
 {
     IFS=$'\n'
-    COMPREPLY=($(${COMP_WORDS[0]} --complete "${COMP_WORDS[@]:1:COMP_CWORD}"))
+    local words cword cur
+    _get_comp_words_by_ref -n = cur words cword # Bypass = in COMP_WORDBREAKS (https://stackoverflow.com/a/57437406/854540)
+    COMPREPLY=($(${words[0]} --complete "${words[@]:1:cword}"))
+    __ltrim_equal_completions "$cur" # Bypass = in COMP_WORDBREAKS (https://stackoverflow.com/a/57437406/854540)
 }
 
 complete -o default -F _demo demo.exe demo_compat.exe
